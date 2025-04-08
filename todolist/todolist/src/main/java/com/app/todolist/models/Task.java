3d3ce0d81160c016c36b13e6b60a27c466f94404
aaronersando <agersando@gmail.com>
2025-04-09 01:47:23 +0800

    Configured Basic Task Record and Task Repository Class

--- todolist/todolist/src/main/java/com/app/todolist/models/Task.java
+++ todolist/todolist/src/main/java/com/app/todolist/models/Task.java
@@ -1,5 +1,46 @@
 package com.app.todolist.models;
 
-public class Task {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.With;
 
+// U Don't actually need lombok if you're using record classes
+// But a benefit of using it even with records is that you can use @Builder and @With annotations that makes it easy
+// to create instances of the class and modify them immutably.
+
+@Builder
+@With
+public record Task(
+    Integer id,
+    @NotEmpty
+    String title,
+    @Positive
+    boolean completed
+) {
 }
+
+// package com.example.demo.run;
+
+// import java.time.LocalDateTime;
+
+// import jakarta.validation.constraints.NotEmpty;
+// import jakarta.validation.constraints.Positive;
+
+// public record Run(
+//     Integer id,
+//     @NotEmpty
+//     String title,
+//     LocalDateTime startedOn,
+//     LocalDateTime completedOn,
+//     @Positive
+//     Integer miles,
+//     Location location
+// ) {
+//     public Run{
+//         if(!completedOn.isAfter(startedOn)){
+//             throw new IllegalArgumentException("Completed On must be after Started On");
+//         }
+//     }
+
+// }
